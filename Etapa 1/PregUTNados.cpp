5607ad530dca051f57d3a40478471dd0e04650aa
gillchristian <gillchristiang@gmail.com>
2015-07-09 22:20:02 -0300

    Commit inicial

---
+++ Etapa 1/PregUTNados.cpp
@@ -0,0 +1,71 @@
+/*
+INTEGRANTES:
+Matias Ezequiel BERTILUZZI
+	DNI: 39294269
+	Email: Maturro1@hotmail.com
+Ignacio Martin ROMANG
+	DNI: 37396357
+	Email: ignacio21496@gmail.com
+Cristian Gabriel GILL DESIA
+	DNI: 35444379
+	Email: gillchristiang@gmail.com
+*/
+
+#include <iostream>
+/* Librerias que podria usar en un futuro
+#include <iomanip>
+#include <string> 
+#include <math.h> 
+#include <stdlib.h>
+*/
+using namespace std;
+//Logo pregUTNados
+void implogo();
+
+int main() {
+	implogo();
+	return 0;
+}
+void implogo(){
+	cout <<
+	"######"<<endl<<
+	"#     #  #####   ######   ####   .ssssss.   .ssssss/    +sssss+ " <<endl<<
+	"#     #  #    #  #       #    #  .MMMMMM+   -MMMMMMs   `NMMMMMy"<<endl<<
+	"######   #    #  #####   #        mMMMMMm.  -MMMMMMs   sMMMMMM/"<<endl<<
+	"#        #####   #       #  ###   +MMMMMMm: -MMMMMMs `sMMMMMMm`"<<endl<<
+	"#        #   #   #       #    #    yMMMMMMNy+MMMMMMhomMMMMMMN- "<<endl<<
+	"#        #    #  ######   ####     yMMMMMMNy+MMMMMMhomMMMMMMN- "<<endl<<
+	"                                   `sMMMMMMMMMMMMMMMMMMMMMMm-  "<<endl<<
+	"#     #  #######  #     #            /dMMMMMMMMMMMMMMMMMMNs. "<<endl<<
+	"#     #     #     ##    #        `....-+dMMMMMMMMMMMMMMNy:....` "<<endl<<
+	"#     #     #     # #   #        :mmmmmmNMMMMMMMMMMMMMMNmmmmmmy "<<endl<<
+	"#     #     #     #  #  #        :mmmmmmNMMMMMMMMMMMMMMNmmmmmmy "<<endl<< 
+	"#     #     #     #  #  #        :MMMMMMMMMMMMMMMMMMMMMMMMMMMMd "<<endl<<
+	"#     #     #     #   # #        :MMMMMMMMMMMMMMMMMMMMMMMMMMMMd "<<endl<<
+	"#     #     #     #    ##        .+++++ohMMMMMMMMMMMMMMmso++++/ "<<endl<<
+	" #####      #     #     #            `+dMMMMMMMMMMMMMMMMms-  "<<endl<<
+	"                                   -dMMMMMMMMMMMMMMMMMMMMN+` "<<endl<<
+	"  ##    #####    ####    #### 	   :NMMMMMMNdMMMMMMmmMMMMMMMy` "<<endl<<
+	" #  #   #    #  #    #  #     	   :NMMMMMMNdMMMMMMmmMMMMMMMy` "<<endl<<
+	"#    #  #    #  #    #   #### 	  .NMMMMMMy--MMMMMMs`+mMMMMMMs "<<endl<<
+	"######  #    #  #    #       #   hMMMMMM+  -MMMMMMs  .mMMMMMM."<<endl<<
+	"#    #  #    #  #    #  #    #	 `MMMMMMy   -MMMMMMs   -MMMMMMo"<<endl<<
+	"#    #  #####    ####    ####   -NNNNNN:   -NNNNNNs    hNNNNNy"<<endl<<
+	"                                                              "<<endl<<
+	"****************************************************************" <<endl<<
+	"************************** Equipo  20 **************************"<<endl<<
+	"****************************************************************"<<endl<<
+    "*********************** Version 0.1 2015 ***********************"<<endl<<
+	"***************************'('indev')'**************************"<<endl<<
+	"****************************************************************"<<endl<<
+	"********** Matias Ezequiel Bertoluzzi **************************"<<endl<<
+    "**************** DNI: 39294269 *********************************"<<endl<<
+	"**************** Email: Maturro1@hotmail.com *******************"<<endl<<
+	"********** Ignacio Martin Romang *******************************"<<endl<<      
+	"**************** DNI: 37396357 *********************************"<<endl<<
+	"**************** Email: ignacio21496@gmail.com *****************"<<endl<<
+	"********** Cristian Gil Desia **********************************"<<endl<< 
+	"**************** DNI: 35444379 *********************************"<<endl<<
+	"**************** Email: gillchristiang@gmail.com ***************"<<endl<<
+    "****************************************************************"<<endl;
+	}
